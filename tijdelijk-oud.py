72a9bbf01a823edd889ae04216147ae9d10b0577
T. van Driel <tcwvandriel@hotmail.com>
2023-08-09 09:37:16 +0200

    functies toegevoegd

---
+++ tijdelijk-oud.py
@@ -0,0 +1,16 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+aanbieding = (int(prijzen ["vanille"])* 0.8)
+reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}")
+reclame_tekst2 = (reclame_tekst[:62])
+reclame_tekst3= (reclame_tekst2.upper())
+reclame_tekst4= (reclame_tekst3.split(" "))
+el= (reclame_tekst2.lower().split(" "))
+for el in reclame_tekst4:
+    if len(el) > 4:
+        print(el.upper())
+    else:
+        print(el.lower())
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,12 +1,22 @@
-prijzen = {
-    "aardbei" : "3",
-    "vanille" : "3",
-    "chocolade" : "5"
-}
-aanbieding = (int(prijzen ["vanille"])* 0.8)
-reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}")
-reclame_tekst2 = (reclame_tekst[:62])
-reclame_tekst3= (reclame_tekst2.upper())
-reclame_tekst4= (reclame_tekst3.split(" "))
-el= (reclame_tekst2.lower().split(" "))
-print (el)
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+    aanbieding = prijzen["aardbei"]* 0.8
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst2 = reclame_tekst[:63]
+    reclame_tekst3= reclame_tekst2.upper()
+    reclame_tekst4= reclame_tekst3.split()
+    el= (reclame_tekst2.lower().split(" "))
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("aanbieding")
+print_aanbieding()
